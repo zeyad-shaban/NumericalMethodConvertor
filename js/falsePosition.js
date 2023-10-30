93e531bcec249a945eed930a837e49a7d0fc31e1
zeyad-shaban <zeyadshapan2004@gmail.com>
2023-10-30 15:05:56 +0200

    fixed falseposition for one itteration

--- js/falsePosition.js
+++ js/falsePosition.js
@@ -4,8 +4,8 @@ let falsePosition = (fx, a, b, preErr, maxIttr) => {
     let i = 0;
 
     do {
-        let fnx1 = math.evaluate(fx, {x: x1});
-        let fnx2 = math.evaluate(fx, {x: x2});
+        let fnx1 = math.evaluate(fx, { x: x1 });
+        let fnx2 = math.evaluate(fx, { x: x2 });
 
         if (i == 0 && fnx1 * fnx2 >= 0) return "ROOT NOT BETWEEN";
 
@@ -13,16 +13,14 @@ let falsePosition = (fx, a, b, preErr, maxIttr) => {
 
         prevX3 = x3;
         x3 = x1 - ((x2 - x1) * fnx1 / (fnx2 - fnx1));
-        let fnx3 = math.evaluate(fx, {x: x3});
-
-        if (fnx3 * fnx1 < 0) x2 = x3;
-        else if (fnx3 * fnx2 < 0) x1 = x3;
-        else {
-            break;
-        }
+        let fnx3 = math.evaluate(fx, { x: x3 });
 
         err = Math.abs(x3 - prevX3);
         addToTable(i + 1, x1, x2, x3, fnx3, err);
+
+        if (fnx3 * fnx1 < 0) x2 = x3;
+        else if (fnx3 * fnx2 < 0) x1 = x3;
+        else break;
         i++;
     } while (err > preErr && i < maxIttr);
 
